6df7ec9bd7d04b965a7839860cf3fad5144cea6c
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-01-05 18:32:14 +0800

    プレイヤーが動くまで

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,24 +3,47 @@
 public class Player : MonoBehaviour
 {
     private PlayerControls controls;
+    private CharacterController characterController;
 
-    public Vector2 moveInput;
-    public Vector2 aimInput;
+    [Header("Movement info")]
+    public float walkSpeed;
+    public Vector3 movementDirection;
+
+    private Vector2 moveInput;
+    private Vector2 aimInput;
 
     private void Awake()
     {
         controls = new PlayerControls();
 
-        controls.Character.Movement.performed += context => moveInput = context.ReadValue< Vector2 >();
+        controls.Character.Movement.performed += context => moveInput = context.ReadValue<Vector2>();
         controls.Character.Movement.canceled += context => moveInput = Vector2.zero;
 
         controls.Character.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
         controls.Character.Aim.canceled += context => aimInput = Vector2.zero;
-
-
     }
 
+    private void Start()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
 
+    private void Update()
+    {
+        ApplyMovement();
+    }
+
+    private void ApplyMovement()
+    {
+        movementDirection = new Vector3(moveInput.x, 0, moveInput.y);
+
+        // Magnitude: ベクトルの大きさ(長さ)
+        // ゼロより大きければ何らかの入力があるということ
+        if (movementDirection.magnitude > 0)
+        {
+            characterController.Move(movementDirection * Time.deltaTime * walkSpeed);
+        }
+    }
 
     private void Shoot()
     {

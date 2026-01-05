8647fdf3883b0b5182deae64af6c64f2fd3cb2ee
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-01-05 18:17:07 +0800

    add new input system

---
+++ Assets/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private PlayerControls controls;
+
+    public Vector2 moveInput;
+    public Vector2 aimInput;
+
+    private void Awake()
+    {
+        controls = new PlayerControls();
+
+        controls.Character.Movement.performed += context => moveInput = context.ReadValue< Vector2 >();
+        controls.Character.Movement.canceled += context => moveInput = Vector2.zero;
+
+        controls.Character.Aim.performed += context => aimInput = context.ReadValue<Vector2>();
+        controls.Character.Aim.canceled += context => aimInput = Vector2.zero;
+
+
+    }
+
+
+
+    private void Shoot()
+    {
+        Debug.Log("shoot");
+    }
+
+
+    private void OnEnable()
+    {
+        controls.Enable();
+    }
+
+    private void OnDisable()
+    {
+        controls.Disable();
+    }
+
+
+}

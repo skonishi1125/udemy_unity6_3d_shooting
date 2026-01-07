fc2c229b8ab510fe26feeeabb02391d9bdf09c19
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-01-07 17:23:59 +0800

    射撃アニメーションの用意

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -46,6 +46,11 @@ public class PlayerMovement : MonoBehaviour
         AnimatorController();
     }
 
+    private void Shoot()
+    {
+        animator.SetTrigger("Fire");
+    }
+
     private void AnimatorController()
     {
         /* 
@@ -121,14 +126,11 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void Shoot()
-    {
-        Debug.Log("shoot");
-    }
-
     private void AssignInputEvents()
     {
         controls = new PlayerControls();
+
+        controls.Character.Fire.performed += context => Shoot();
 
         controls.Character.Movement.performed += context => moveInput = context.ReadValue<Vector2>();
         controls.Character.Movement.canceled += context => moveInput = Vector2.zero;

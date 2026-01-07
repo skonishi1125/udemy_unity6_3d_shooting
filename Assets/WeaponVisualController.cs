fc1b3e6aa39d04aa69a7ef75403ebaee88ba0726
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-01-07 18:18:13 +0800

    銃の切り替え処理の実装

---
+++ Assets/WeaponVisualController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponVisualController : MonoBehaviour
+{
+    [SerializeField] private Transform[] gunTransforms;
+
+    [SerializeField] private Transform pistol;
+    [SerializeField] private Transform revolver;
+    [SerializeField] private Transform autoRifle;
+    [SerializeField] private Transform shotgun;
+    [SerializeField] private Transform rifle;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+            SwitchOn(pistol);
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+            SwitchOn(revolver);
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+            SwitchOn(autoRifle);
+
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+            SwitchOn(shotgun);
+
+        if (Input.GetKeyDown(KeyCode.Alpha5))
+            SwitchOn(rifle);
+    }
+
+    private void SwitchOn(Transform gunTransform)
+    {
+        SwitchOffGuns();
+        gunTransform.gameObject.SetActive(true);
+    }
+
+    private void SwitchOffGuns()
+    {
+        for (int i = 0; i < gunTransforms.Length; i++)
+        {
+            gunTransforms[i].gameObject.SetActive(false);
+        }
+    }
+}

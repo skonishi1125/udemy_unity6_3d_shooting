5eef0cd1c7fa7549b0f5d87feabc6152c8be0f22
SKONI_NOTE\watas <skonishi1125@gmail.com>
2026-01-08 19:35:41 +0800

    Cinemachineの導入

---
+++ Assets/Scripts/PlayerAnimationEvent.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimationEvent : MonoBehaviour
+{
+    private PlayerWeaponVisuals visualController;
+
+    private void Start()
+    {
+        visualController = GetComponentInParent<PlayerWeaponVisuals>();
+    }
+
+    public void ReloadIsOver()
+    {
+        visualController.MaximizeRigWeight();
+
+        // リロードが完了したら、弾丸を増やすような処理を書く
+    }
+
+    // 武器切り替え
+    public void WeaponGrabIsOver()
+    {
+
+
+        visualController.SetBusyGrabbingWeaponTo(false);
+    }
+
+    public void ReturnRig()
+    {
+        visualController.MaximizeRigWeight();
+        visualController.MaximizeLeftHandWeight(); // 左手も操作するので、そのリグの調整(再び銃に手を添える)
+    }
+
+
+}
